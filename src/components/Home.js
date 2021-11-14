c6c4934672788024f96e7b68964f39e3d249a2f2
NamanMalhotra2001 <74060787+NamanMalhotra2001@users.noreply.github.com>
2021-11-14 17:19:17 +0530

    restore

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+// Config
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../API_config";
+
+// Components
+
+// Hooks
+
+// Image
+import NoImage from "../images/no_image.jpg";
+
+export default function Home() {
+	return (
+		<div>
+			
+		</div>
+	)
+}
