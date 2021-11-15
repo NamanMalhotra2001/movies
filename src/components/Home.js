91c6ccd8b5e9b835042957fdd24e11a60adaf248
NamanMalhotra2001 <74060787+NamanMalhotra2001@users.noreply.github.com>
2021-11-15 18:42:27 +0530

    vid 20

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,46 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react';
+// API
+import API from '../API';
 // Config
-import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../API_config";
-
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 // Components
 
-// Hooks
+// Hook
 
 // Image
-import NoImage from "../images/no_image.jpg";
+import NoImage from '../images/no_image.jpg';
 
-export default function Home() {
-	return (
-		<div>
-			
-		</div>
-	)
-}
+const Home = () => {
+  const [state, setState] = useState();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const fetchMovies = async (page, searchTerm = '') => {
+    try {
+      setError(false);
+      setLoading(true);
+
+      const movies = await API.fetchMovies(searchTerm, page);
+
+      setState(prev => ({
+        ...movies,
+        results:
+          page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
+      }));
+    } catch (error) {
+      setError(true);
+    }
+    setLoading(false);
+  };
+
+  // Initial render
+  useEffect(() => {
+    fetchMovies(1);
+  }, []);
+
+  console.log(state);
+
+  return <div>Home Page</div>;
+};
+
+export default Home;
